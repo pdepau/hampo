3da5696bdfd06886ea58261d5fc6fe31799ea74a
Adrián <admallla@epsg.upv.es>
2020-11-19 21:48:33 +0100

    Vista del mapa.

--- general/Hampo/app/src/main/java/com/example/hampo/presentacion/CreateHampoActivity.java
+++ general/Hampo/app/src/main/java/com/example/hampo/presentacion/CreateHampoActivity.java
@@ -126,7 +126,7 @@ public class CreateHampoActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_mis_hampos, R.id.nav_mi_perfil, R.id.nav_config, R.id.nav_adiestramiento, R.id.nav_FAQ)
+                R.id.nav_mis_hampos, R.id.nav_mi_perfil, R.id.nav_config, R.id.nav_tinder, R.id.nav_FAQ)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
