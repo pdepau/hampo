5f00f215e034a0ae684d3618613a43e535061d1f
Lorena Florescu <lorelaywilson@gmail.com>
2021-01-20 12:14:53 +0100

    pref y cambios en edit y crear

--- general/Hampo/app/src/main/java/com/example/hampo/presentacion/PreferenciasActivity.java
+++ general/Hampo/app/src/main/java/com/example/hampo/presentacion/PreferenciasActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -23,12 +24,7 @@ public class PreferenciasActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new PreferenciasFragment())
                 .commit();
-        /*Intent Email = new Intent(Intent.ACTION_SEND);
-        Email.setType("text/email");
-        Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "admin@hotmail.com" });
-        Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-        Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "");
-        startActivity(Intent.createChooser(Email, "Send Feedback:"));*/
+
 
     }
 
@@ -36,6 +32,20 @@ public class PreferenciasActivity extends AppCompatActivity {
     public void onBackPressed(){
         startActivity(new Intent(this, MainActivity.class));
         finish();
+    }
+
+    public void lanzarPrivacidad(View v){
+        Intent i = new Intent(this, PrivacidadActivity.class);
+        startActivity(i);
+    }
+
+    public void mandarCorreo(){
+         Intent Email = new Intent(Intent.ACTION_SEND);
+        Email.setType("text/email");
+        Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "admin@hotmail.com" });
+        Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+        Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "");
+        startActivity(Intent.createChooser(Email, "Send Feedback:"));
     }
 
 
---
+++ general/Hampo/app/src/main/java/com/example/hampo/presentacion/PrivacidadActivity.java
@@ -0,0 +1,17 @@
+package com.example.hampo.presentacion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hampo.R;
+
+public class PrivacidadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.privacidad);
+
+    }
+}
